536e12162595d977690caf12bcc935ef4c78e35a
cla6shade <cla6shade@gmail.com>
2025-03-24 02:31:26 +0900

    chore: setup express and logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import text from '@/module';
+import express from 'express';
+import logger from '@/utils/logger';
 
-console.log(text);
+const app = express();
+const PORT = process.env.SERVER_PORT || 3000;
+
+app.use(logger);
+app.use(express.json());
+
+app.get('/', (_, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+});
