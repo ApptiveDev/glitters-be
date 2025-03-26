effd9d29da884e5fc487932a2f1872a6e66a1130
cla6shade <cla6shade@github.com>
2025-03-26 16:02:12 +0900

    fix: pnpm 관련 prisma 오류 해결

--- src/utils/database/index.ts
+++ src/utils/database/index.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '.prisma/client';
 
-const prismaClient = new PrismaClient();
-export default prismaClient;
+const prisma = new PrismaClient();
+
+export default prisma;
