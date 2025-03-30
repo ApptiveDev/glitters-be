1e7a01c72204682e56a7193cef1ecdfc1ccbc3c6
cla6shade <cla6shade@gmail.com>
2025-03-31 02:44:16 +0900

    feat: 스키마 에러일 경우 BAD_REQUEST를 보내도록 설정

--- src/utils/network/index.ts
+++ src/utils/network/index.ts
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import { Response } from 'express';
+import { z } from 'zod';
 
 export function sendError(res: Response, message: string, errorCode: number = StatusCodes.INTERNAL_SERVER_ERROR) {
   res.status(errorCode).json({
@@ -8,6 +9,10 @@ export function sendError(res: Response, message: string, errorCode: number = St
 }
 
 export function sendAndTrace(res: Response, error: any) {
+  if(error instanceof z.ZodError) {
+    sendError(res, '잘못된 요청입니다.', StatusCodes.BAD_REQUEST);
+    return;
+  }
   sendError(res, '서버 에러가 발생했습니다.');
   console.error(error);
 }
