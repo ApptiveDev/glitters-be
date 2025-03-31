8c4f3e1440285e40c002a41fee7c904bb226de08
cla6shade <cla6shade@gmail.com>
2025-03-31 13:57:33 +0900

    feat: 이메일 인증 횟수 제한 구현

--- src/domains/auth/middleware.ts
+++ src/domains/auth/middleware.ts
@@ -6,6 +6,25 @@ import { verifyToken } from '@/domains/auth/utils';
 import prisma from '@/utils/database';
 import { sendAndTrace, sendError } from '@/utils/network';
 import { StatusCodes } from 'http-status-codes';
+import rateLimit from 'express-rate-limit';
+
+const oneDayInMs = 24 * 60 * 60 * 1000;
+
+export function verifyEmailRequestLimiter() {
+  return rateLimit({
+    windowMs: oneDayInMs,
+    limit: 5,
+    message: '이메일 인증 요청은 하루에 최대 5번까지만 가능합니다.',
+  });
+}
+
+export function verifyEmailCodeLimiter() {
+  return rateLimit({
+    windowMs: oneDayInMs,
+    limit: 5,
+    message: '인증 코드는 하루에 최대 5번까지만 입력할 수 있습니다.',
+  });
+}
 
 export async function authMiddleware(
   req: AuthenticatedRequest,
