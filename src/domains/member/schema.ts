036a4aea47c52a6102a8c2387329f0fdca9ec460
cla6shade <cla6shade@github.com>
2025-04-03 16:55:20 +0900

    feat: 마지막 자신이 작성한 게시글을 반환하는 api 생성

--- src/domains/member/schema.ts
+++ src/domains/member/schema.ts
@@ -1,8 +1,11 @@
-// import { z } from 'zod';
-import { MemberSchema } from '@/schemas';
+import { MemberSchema, PostSchema } from '@/schemas';
 import { z } from 'zod';
 
 export const PasswordExcludedMemberSchema = MemberSchema.omit({ password: true });
 export const GetMyInfoSchema = z.object({
   member: PasswordExcludedMemberSchema,
+});
+
+export const GetLastPostResponseBodySchema = PostSchema.pick({
+  createdAt: true,
 });
