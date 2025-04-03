3e679be728941b45d2879b7dc63bf1c65401945a
cla6shade <cla6shade@github.com>
2025-04-03 17:07:04 +0900

    feat: last_created 엔드포인트 등록

--- src/domains/member/controller.ts
+++ src/domains/member/controller.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authMiddleware } from '@/domains/auth/middleware';
-import { getMyInfo } from '@/domains/member/service';
+import { getLastCreatedPost, getMyInfo } from '@/domains/member/service';
 
 const memberRouter = express.Router();
 
 const ROUTE_PREFIX = '/members';
 
 memberRouter.get(`${ROUTE_PREFIX}/me`, authMiddleware, getMyInfo);
+memberRouter.get(`${ROUTE_PREFIX}/last_created`, authMiddleware, getLastCreatedPost);
 
 export default memberRouter;
