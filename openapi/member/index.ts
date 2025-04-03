893cdd48ee8fcb31d33a3a749fb97026739b397e
cla6shade <cla6shade@github.com>
2025-04-03 17:06:26 +0900

    docs: 게시글의 생성시간 반환 api

--- openapi/member/index.ts
+++ openapi/member/index.ts
@@ -1,9 +1,10 @@
 import { currentApiPrefix } from '../../src/constants';
+import { tokenHeader } from '../headers';
 import {
   GetMyInfoSchema,
+  GetLastPostResponseBodySchema,
 } from '../../src/domains/member/schema';
 import { ErrorSchema } from '../../src/domains/error/schema';
-import { tokenHeader } from '../headers';
 
 export const memberApiPaths = {
   [`${currentApiPrefix}/members/me`]: {
@@ -16,7 +17,33 @@ export const memberApiPaths = {
           description: '회원 정보 반환',
           content: {
             'application/json': {
-              schema: GetMyInfoSchema
+              schema: GetMyInfoSchema,
+            },
+          },
+        },
+        401: {
+          description: '인증 실패 또는 토큰 누락',
+          content: {
+            'application/json': {
+              schema: ErrorSchema,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  [`${currentApiPrefix}/members/last_created`]: {
+    get: {
+      summary: '마지막 게시글 생성시간 조회',
+      security: [{ bearerAuth: [] }],
+      parameters: [tokenHeader],
+      responses: {
+        200: {
+          description: '최근 게시글 생성시간 반환',
+          content: {
+            'application/json': {
+              schema: GetLastPostResponseBodySchema,
             },
           },
         },
