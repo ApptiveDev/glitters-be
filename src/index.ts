965c70649ce789f8c830ddc33c7eee79e406311b
cla6shade <cla6shade@gmail.com>
2025-04-08 01:58:28 +0900

    fix: 이메일 요청 시 요청 횟수가 제대로 집계되지 않던 문제점 해결

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,8 @@ app.use(API_PREFIX, markerRouter);
 app.use(API_PREFIX, postRouter);
 app.use(API_PREFIX, reportRouter);
 
+app.set('trust proxy', 1);
+
 app.get('/', (_, res) => {
   res.send('Hello, Express!');
 });
