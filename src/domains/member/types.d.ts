b976eb6b61445b44658da2097b675da11278b18b
cla6shade <cla6shade@github.com>
2025-05-06 18:44:11 +0900

    refactor: request 내부 저장용 member와 전송용 member의 스키마, 타입 분리

--- src/domains/member/types.d.ts
+++ src/domains/member/types.d.ts
@@ -1,11 +1,14 @@
-import type { Member } from '.prisma/client';
 import type { Response } from 'express';
 import {
   GetActivePostCountResponseBodySchema,
   GetMyInfoSchema,
+  InternalMemberSchema,
+  PublicMemberSchema,
 } from '@/domains/member/schema';
+import { z } from 'zod';
 
-export type PasswordExcludedMember = Omit<Member, 'password'>;
+export type PublicMember = z.infer<typeof PublicMemberSchema>;
+export type InternalMember = z.infer<typeof InternalMemberSchema>;
 export type GetMyInfoResponse = Response<z.infer<typeof GetMyInfoSchema>>
 
 export type GetActivePostCountResponse = Response<z.infer<typeof GetActivePostCountResponseBodySchema>>;
