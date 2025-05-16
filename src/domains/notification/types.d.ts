0525fb35de6c6e60f7120fec424820df7082a2b8
cla6shade <cla6shade@github.com>
2025-05-16 11:56:04 +0900

    feat: 푸쉬메시지 전송 시 푸쉬메시지의 타입을 함께 포함하여 전송하도록 변경

--- src/domains/notification/types.d.ts
+++ src/domains/notification/types.d.ts
@@ -7,3 +7,7 @@ import { z } from 'zod';
 
 export type LocationInputRequest = AuthenticatedRequest<{}, z.infer<typeof LocationInputRequestBodySchema>>;
 export type ExpoTokenInputRequest = AuthenticatedRequest<{}, z.infer<typeof ExpoTokenInputRequestBodySchema>>;
+
+export interface NotificationData extends Record<string, unknown> {
+  type: 'chat' | 'nearby';
+}
