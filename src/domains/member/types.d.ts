efd09f3e805b148b6f025a0109fcac1485bbcb0f
cla6shade <cla6shade@gmail.com>
2025-05-18 22:37:59 +0900

    refactor: req 내부에 멤버 정보를 JWT에서 뽑아 저장하도록 변경

--- src/domains/member/types.d.ts
+++ src/domains/member/types.d.ts
@@ -2,13 +2,13 @@ import type { Response } from 'express';
 import {
   GetActivePostCountResponseBodySchema,
   GetMyInfoSchema,
-  InternalMemberSchema,
   PublicMemberSchema,
 } from '@/domains/member/schema';
 import { z } from 'zod';
+import { AuthenticatedJWTPayload } from '@/domains/auth/types';
 
 export type PublicMember = z.infer<typeof PublicMemberSchema>;
-export type InternalMember = z.infer<typeof InternalMemberSchema>;
+export type InternalMember = Pick<AuthenticatedJWTPayload, 'id' | 'name' | 'email'>;
 export type GetMyInfoResponse = Response<z.infer<typeof GetMyInfoSchema>>
 
 export type GetActivePostCountResponse = Response<z.infer<typeof GetActivePostCountResponseBodySchema>>;
