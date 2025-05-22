0349c81e06fe08023072fe891398824412668228
cla6shade <cla6shade@github.com>
2025-05-22 09:14:38 +0900

    chore: 게시글 자동생성 로직 개선

--- scripts/mock/createPost.ts
+++ scripts/mock/createPost.ts
@@ -1,42 +1,42 @@
 import prisma from '../../src/utils/database';
 
 const titles = [
-  '새벽벌도서관 1층에서 댄디한 정장 차림의 잘생긴 사람',
-  '새벽벌도서관 2층에서 눈웃음이 예쁜 사람, 흰 셔츠를 입고 있었음',
-  '새벽벌도서관 3층에서 금발 염색에 청바지를 입은 사람',
-  '새벽벌도서관 4층에서 검은 슬랙스를 입은 키 큰 사람',
-  '새벽벌도서관 입구에서 단정한 셔츠에 얼굴 작은 사람',
-  '새벽벌도서관 편의점 앞에서 꾸안꾸 스타일의 예쁜 사람',
-  '새벽벌도서관 2층에서 흰 후드티와 캡모자를 쓴 훈훈한 사람',
-  '새벽벌도서관 3층에서 긴 생머리에 베이지 코트를 입은 사람',
-  '새벽벌도서관 1층에서 검은 셔츠를 입은 날카로운 인상의 사람',
-  '새벽벌도서관 입구에서 운동복 차림의 건강미 있는 사람'
+  '정장 입고 계신분',
+  '순버 맨 앞자리에 계시던 여성분',
+  '금발에 청바지 입은 남성분',
+  '검은 슬랙스를 입은 키 큰 남성분',
+  '단정한 셔츠에 얼굴 작은 사람',
+  '흰색 티셔츠에 모자쓰신분',
+  '흰 후드티에 조거팬츠 입으신 남성분',
+  '분홍색 니트에 슬랙스 입으신 여성분',
+  '검은 셔츠 입고계신 남성분',
+  '운동복 입고 친구들과 같이 가시던 여성분'
 ];
 
 const contents = [
   '비율이 너무 좋아서 반짝였어요.',
-  '웃는 모습이 예뻐서 반짝였어요.',
+  '웃는 모습이 반짝였어요.',
   '헤어스타일이 완벽해서 반짝였어요.',
   '피지컬이 좋아서 반짝였어요.',
   '얼굴형이 정말 예뻐서 반짝였어요.',
-  '스타일이 딱 제 취향이라 반짝였어요.',
+  '스타일이 제 취향이라 반짝였어요.',
   '자연스럽게 멋져 보여서 반짝였어요.',
-  '머리부터 발끝까지 조화로워서 반짝였어요.',
+  '옷을 너무 잘 입으셔서 반짝였어요.',
   '차가운 인상이 매력 있어서 반짝였어요.',
-  '건강한 분위기에서 눈을 뗄 수 없어서 반짝였어요.'
+  '눈매가 제 스타일이셔서 반짝였어요.'
 ];
 
 async function createRandomPostAndMarker() {
   const randomTitle = titles[Math.floor(Math.random() * titles.length)];
   const randomContent = contents[Math.floor(Math.random() * contents.length)];
-  const randomAddress = '부산대학교 새벽벌도서관';
-  const authorIds = [7];
+  const randomAddress = '부산대학교 강의실';
+  const authorIds = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   const markerIdx = Math.floor(Math.random() * 4);
   const randomAuthorId = authorIds[Math.floor(Math.random() * authorIds.length)];
 
   const baseLat = 35.235789;
   const baseLon = 129.081399;
-  const noise = () => (Math.random() - 0.5) * 0.001;
+  const noise = () => (Math.random() - 0.5) * 0.009;
 
   const marker = await prisma.marker.create({
     data: {
@@ -75,9 +75,10 @@ async function main() {
     }
   }
 
-  for (let i = 0; i < count; i++) {
-    await createRandomPostAndMarker();
-  }
+  await Promise.all(
+    Array.from({ length: count }, () => createRandomPostAndMarker())
+  );
+
 }
 
 main()
